5a92b65506e698110849fec00a42e9471e4c482b
Alex-Vicente11 <teiker_01@hotmail.com>
2026-04-16 00:02:49 -0600

    Formatear y ajustar fecha para visualizar al inicio de la pantalla

--- app/src/main/java/com/juandgaines/todoapp/presentation/screens/home/HomeScreenViewModel.kt
+++ app/src/main/java/com/juandgaines/todoapp/presentation/screens/home/HomeScreenViewModel.kt
@@ -15,6 +15,8 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class HomeScreenViewModel:ViewModel() {
 
@@ -27,13 +29,19 @@ class HomeScreenViewModel:ViewModel() {
     val events = eventChannel.receiveAsFlow()
 
     init {
+
+        state = state.copy(
+            date = LocalDate.now().let {
+                DateTimeFormatter.ofPattern("EEEE, MMMM dd, yyyy").format(it)
+            }
+        )
+
         taskLocalDataSource.tasksFlow.onEach {
             val completedTasks = it.filter { task -> task.isCompleted }
             val pendingTasks = it.filter { task -> !task.isCompleted }
 
-            state = HomeDataState(
-                date = "Today",
-                summary = "You have ${pendingTasks.size} pending tasks",
+            state = state.copy(
+                summary = pendingTasks.size.toString(),
                 completedTask = completedTasks,
                 pendingTask = pendingTasks,
             )
--- app/src/main/java/com/juandgaines/todoapp/presentation/screens/home/components/SummaryInfo.kt
+++ app/src/main/java/com/juandgaines/todoapp/presentation/screens/home/components/SummaryInfo.kt
@@ -18,9 +18,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.juandgaines.todoapp.R
 import com.juandgaines.todoapp.ui.theme.TodoAppTheme
 
 @Composable
@@ -58,6 +60,7 @@ fun SummaryInfo(
         Column(
             modifier = modifier
                 .padding(16.dp)
+                .weight(1.5f)
         ) {
             Text(
                 text = date,
@@ -67,7 +70,7 @@ fun SummaryInfo(
             )
 
             Text(
-                text = tasksSummary,
+                text = stringResource(R.string.summary_info, tasksSummary),
                 style = MaterialTheme.typography.titleSmall,
                 color = MaterialTheme.colorScheme.onSurface,
             )
@@ -77,6 +80,7 @@ fun SummaryInfo(
             modifier = Modifier
                 .padding(16.dp)
                 .aspectRatio(1f)
+                .weight(1f)
         ) {
             val colorBase = MaterialTheme.colorScheme.inversePrimary
             val progress = MaterialTheme.colorScheme.primary
